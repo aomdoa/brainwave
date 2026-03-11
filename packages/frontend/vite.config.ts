e6e214f0401d07a97c0527ede3965580273478c3
David Shurgold <aomdoa@gmail.com>
2026-03-11 14:17:29 -0400

    Added basic self signed ssl support

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/brainwave/',
   build: {
     sourcemap: false,
+  },
+  server: {
+    host: true, // allows LAN access if needed
+    https: {
+      key: readFileSync(resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: readFileSync(resolve(__dirname, 'certs/localhost.pem')),
+    },
   },
   plugins: [vue()],
 })
