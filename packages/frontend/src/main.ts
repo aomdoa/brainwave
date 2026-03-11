1fd9023902067aecc7251dac1631bee3fabdd42e
David Shurgold <aomdoa@gmail.com>
2026-03-11 14:12:36 -0400

    Ughh... so much failure going to try again tomorrow

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite-plugin-pwa/client" />
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
@@ -7,6 +8,22 @@ import PrimeVue from 'primevue/config'
 import { DatePicker, MultiSelect, Select, AutoComplete, Dialog } from 'primevue'
 import Aura from '@primeuix/themes/aura'
 import 'primeicons/primeicons.css'
+//import { registerSW } from 'virtual:pwa-register'
+
+/*registerSW({
+  immediate: true,
+})
+
+navigator.serviceWorker.ready.then(() => {
+  console.log('[Page] Service worker is ready and controlling this page!')
+})
+
+navigator.serviceWorker.addEventListener('message', (event) => {
+  console.dir(event)
+  if (event.data?.type === 'push') {
+    console.log('[Page] Push received from SW:', event.data.data)
+  }
+})*/
 
 const app = createApp(App)
 app.config.errorHandler = (err, instance, info) => {
