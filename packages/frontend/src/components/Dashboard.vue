56213e2328c83ee13c32a7f0d15fe8e522c3ee42
David Shurgold <aomdoa@gmail.com>
2026-03-13 13:56:56 -0400

    Couple of tidies

--- packages/frontend/src/components/Dashboard.vue
+++ packages/frontend/src/components/Dashboard.vue
@@ -82,7 +82,6 @@ const selectionChange = () => {
 }
 
 const fetchThoughts = async () => {
-  console.dir(currentUser.value)
   const { thoughts: fetchedThoughts, page } = await getThoughts({
     orderBy: {
       field: pagination.orderBy,
